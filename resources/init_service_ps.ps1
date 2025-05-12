7d3ea9231f53c189677711333d9255324df16072
soumyo001 <www.soumyo@gmail.com>
2025-05-12 23:46:40 +0600

    misc

--- resources/init_service_ps.ps1
+++ resources/init_service_ps.ps1
@@ -1,22 +1,39 @@
 # script must run as admin
 $nssmUrl = "https://nssm.cc/release/nssm-2.24.zip"  # URL to download NSSM
 $nssmFolder = "C:\nssm" # modify this path based on needs
+$nssmexe = "$nssmFolder\nssm-2.24\win64\nssm.exe" # for 64-bit systems
 $scriptPath = "C:\path\to\your\script.ps1"  # path to save the downloaded PowerShell script
 $serviceName = "MyService"
 $exePath = "powershell.exe"
 $arguments = "-ep bypass -noP -File $scriptPath"
 
 $downloadPath = "$nssmFolder\nssm.zip"
-Invoke-WebRequest -Uri $nssmUrl -OutFile $downloadPath
 
-Expand-Archive -Path $downloadPath -DestinationPath $nssmFolder
+if(-not(Test-Path -Path $downloadPath)){
+    Invoke-WebRequest -Uri $nssmUrl -OutFile $downloadPath
+}
+if(-not(Test-Path -Path $nssmFolder -PathType Container)){
+    Expand-Archive -Path $downloadPath -DestinationPath $nssmFolder
+}
 
-Start-Process -FilePath "$nssmFolder\nssm-2.24\win64\nssm.exe" -ArgumentList "install", $serviceName, $exePath, $arguments
+if(Get-Service $ServiceName -ErrorAction SilentlyContinue){
+    & $nssmexe stop $serviceName
+    & $nssmexe remove $serviceName confirm
+}
 
-sc.exe config $serviceName obj=LocalSystem
-sc.exe config $serviceName start=auto
+# Start-Process -FilePath "$nssmFolder\nssm-2.24\win64\nssm.exe" -ArgumentList "install", $serviceName, $exePath, $arguments
+# sc.exe config $serviceName obj=LocalSystem
+# sc.exe config $serviceName start=auto
+# Get-Service -Name $serviceName
+# Start-Service -Name $serviceName
 
-Get-Service -Name $serviceName
-Start-Service -Name $serviceName
+& $nssmexe install $serviceName $exePath $arguments
+
+& $nssmexe set $serviceName Start SERVICE_AUTO_START
+& $nssmexe set $serviceName ObjectName "LocalSystem"
+& $nssmexe set $serviceName AppExit Default Exit
+& $nssmexe set $serviceName AppExit 0 Exit 
+
+& $nssmexe start $serviceName
 
 Write-Host "Service '$serviceName' has been created and started successfully!"
