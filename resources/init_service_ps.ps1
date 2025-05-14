6eb6a28282d084ea13c08324025cc2d3d5b89f40
soumyo001 <www.soumyo@gmail.com>
2025-05-14 12:12:16 +0600

    miscx7

--- resources/init_service_ps.ps1
+++ resources/init_service_ps.ps1
@@ -49,6 +49,7 @@ Write-Host "Service '$serviceName' has been created and started successfully!"
 # Set service SDDL to allow ONLY SYSTEM access
 $SDDL = "O:SYD:(A;;CCDCLCSWRPWPDTLOCRSDRCWDWO;;;SY)"
 sc.exe sdset $serviceName $SDDL
+sc.exe sdshow $ServiceName
 
 # Remove inheritance and lock permissions except current user and SYSTEM
 # icacls $nssmexe /inheritance:r /grant:r "SYSTEM:F" /grant:r "$env:USERNAME:F"
